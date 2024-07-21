00764078ac9ec1b00d850dffbc804b37753f8e26
Rockylars <rockylars@hotmail.com>
2024-07-21 17:12:37 +0200

    Made it functional! But it is not done yet

--- src/PackageParser.php
+++ src/PackageParser.php
@@ -35,34 +35,45 @@ final class PackageParser
     {
         //TODO: Add a config to prevent deep searches (or all searches) in certain arrays, such as vendor.
         $projectRoot = $projectRoot === null ? \Safe\getcwd() : \Safe\realpath($projectRoot);
-        $projectContents = self::search($projectRoot, 1, $searchDepth);
-        self::processGitIgnoreFiles($projectContents);
+
+        //TODO: Test what happens with an empty root directory, we will need to not add an extra separator in those cases, such as the search and the rule processing.
+        //$projectRoot = $projectRoot ?: null;
+        if ($projectRoot === '') {
+            throw new Exception('Add options for this, make sure to not add an extra directory separator');
+        }
+
+        $projectContents = self::search($projectRoot, $searchDepth);
+        $projectContentsList = self::flattenDirectory($projectContents, $searchDepth);
+        self::processGitRulesFiles($projectContents, $projectContentsList, false);
         //TODO: Remove these below later.
         $projectContents['.idea']['included'] = false;
         $projectContents['output']['included'] = false;
         $projectContents['vendor']['included'] = false;
+        //TODO: Test what happens if you ignore a gitkeep file, what will happen?
         self::removeExcludedContent($projectContents);
-        self::processGitAttributesFiles($projectContents);
+        self::processGitRulesFiles($projectContents, $projectContentsList, true);
         self::removeExcludedContent($projectContents);
         //TODO: Add an alternative return where it is a one dimensional array of the full path.
-        return self::summarize($projectContents, 1, $resultDepth);
+        return self::summarize($projectContents, $resultDepth);
     }
 
     /**
-     * @param non-empty-string $directory
-     * @param int<1, max> $currentDepth
+     * @param non-empty-string $directoryPath
      * @param int<1, max> $maxDepth
-     * @return array<int, mixed>
-     * @throws Exception
+     * @param int<1, max> $currentDepth
+     * @param array<int<0, max>, non-empty-string> $route
+     * @param string $localizedDirectoryPath
+     * @return array<non-empty-string, mixed>
+     * @throws DirException
      */
-    private static function search(string $directory, int $currentDepth, int $maxDepth): array
+    private static function search(string $directoryPath, int $maxDepth, int $currentDepth = 1, array $route = [], string $localizedDirectoryPath = ''): array
     {
         /** @var array<int, string> $contents */
-        $contents = \Safe\scandir($directory);
+        $contents = \Safe\scandir($directoryPath);
         $contentCount = count($contents);
 
         $foldersExcluded = ['.', '..'];
-        if ($currentDepth === 1) {
+        if ($isInProjectRoot = $currentDepth === 1) {
             $foldersExcluded[] = '.git';
         }
 
@@ -72,64 +83,105 @@ final class PackageParser
             if (in_array($fileOrFolderName, $foldersExcluded, true)) {
                 continue;
             }
+            // TODO: Test issues with string integers folder names.
+            $localizedPath = $isInProjectRoot ? $fileOrFolderName : $localizedDirectoryPath . DIRECTORY_SEPARATOR . $fileOrFolderName;
             $parsedContents[$fileOrFolderName] = [
-                'is_directory' => $isDir = is_dir($path = $directory . DIRECTORY_SEPARATOR . $fileOrFolderName),
+                'is_directory' => $isDir = is_dir($path = $directoryPath . DIRECTORY_SEPARATOR . $fileOrFolderName),
                 'path' => $path,
-                'included' => true
+                'localized_path' => $localizedPath,
+                'included' => true,
+                'route' => $deeperRoute = array_merge($route, [$fileOrFolderName])
             ];
             if ($isDir && $currentDepth < $maxDepth) {
-                $parsedContents[$fileOrFolderName]['contents'] = self::search($path, $currentDepth + 1, $maxDepth);
+                $parsedContents[$fileOrFolderName]['contents'] = self::search($path, $maxDepth, $currentDepth + 1, $deeperRoute, $localizedPath);
             }
         }
         return $parsedContents;
     }
 
-    private static function processGitIgnoreFiles(array &$directory): void
+    /**
+     * @param array<non-empty-string, mixed> $directory
+     * @param int<1, max> $currentDepth
+     * @param int<1, max> $maxDepth
+     * @return array<int, mixed>
+     */
+    private static function flattenDirectory(array &$directory, int $maxDepth, int $currentDepth = 1): array
     {
-        if (array_key_exists('.gitignore', $directory)) {
-            var_dump('---------------');
-            $lines = explode("\n", \Safe\file_get_contents($directory['.gitignore']['path']));
+        $flatList = [];
+        foreach ($directory as $fileOrFolderName => $info) {
+            $flatList[$info['localized_path']] = [
+                'localized_route' => $info['route'],
+                // Do not replace $directory[$fileOrFolderName] with $info, we're trying to create a reference here.
+                'data' => &$directory[$fileOrFolderName]
+            ];
+            if ($info['is_directory'] && $currentDepth < $maxDepth) {
+                // Do not replace $directory[$fileOrFolderName] with $info, we're trying to create a reference here.
+                $flatList = array_merge($flatList, self::flattenDirectory($directory[$fileOrFolderName]['contents'], $maxDepth, $currentDepth + 1));
+            }
+        }
+        return $flatList;
+    }
+
+    /**
+     * @param array<non-empty-string, mixed> $directory
+     * @param array<non-empty-string, array<int<0, max>, non-empty-string>> $flatList
+     * @throws FilesystemException
+     */
+    private static function processGitRulesFiles(array &$directory, array &$flatList, bool $isSecondRoundForGitAttributes): void
+    {
+        $gitRulesFileName = $isSecondRoundForGitAttributes ? '.gitattributes' : '.gitignore';
+        if (array_key_exists($gitRulesFileName, $directory)) {
+            //var_dump('- FILE - ' . $directory[$gitRulesFileName]['path']);
+            $lines = explode("\n", \Safe\file_get_contents($directory[$gitRulesFileName]['path']));
             foreach ($lines as $line) {
-                $rule = RuleParser::run($line, false);
+                $rule = RuleParser::run($line, $isSecondRoundForGitAttributes);
                 if ($rule === null) {
                     continue;
                 }
-                self::processRule($directory, $rule);
+                self::processRule($directory, $flatList, $rule);
             }
         }
         foreach ($directory as $fileOrFolderName => $info) {
             // The search depth will make some directories not fetch their contents.
             if ($info['is_directory'] && isset($info['contents'])) {
                 // Do not replace $directory[$fileOrFolderName] with $info, we're trying to create a reference here.
-                self::processGitIgnoreFiles($directory[$fileOrFolderName]['contents']);
+                self::processGitRulesFiles($directory[$fileOrFolderName]['contents'], $flatList, $isSecondRoundForGitAttributes);
             }
         }
     }
 
-    private static function processGitAttributesFiles(array &$directory): void
+    /**
+     * @param array<non-empty-string, mixed> $directory
+     * @param array<non-empty-string, array<non-empty-string, mixed> $flatList
+     * @throws PcreException
+     */
+    private static function processRule(array &$directory, array &$flatList, PathMatcher $rule): void
     {
-        if (array_key_exists('.gitattributes', $directory)) {
-            $lines = explode("\n", \Safe\file_get_contents($directory['.gitattributes']['path']));
-            foreach ($lines as $line) {
-                $rule = RuleParser::run($line, true);
-                if ($rule === null) {
+        foreach ($flatList as $localizedFileOrFolderPath => $info) {
+            if ($rule->targetsMatching()) {
+                // TODO: Do the preg match and all "matches + is dir if dir targeting is on" will be marked as "not included"
+            } else {
+                // TODO: Do the preg match and all either "not matches" or "matches but is not directory while dir targeting" will be marked as "included"
+                // TODO: Go from the root to the not matched file/folder to mark each of those directories as "included" to ensure an ignore plus include pattern will work, but you only have to do this once.
+            }
+
+            // TODO: Switch it for the local directory so we don't wrongly target the incorrect files.
+            $matches = [];
+            if (\Safe\preg_match('/'. $rule->asRegExp() . '/u', $localizedFileOrFolderPath, $matches)) {
+                // You can have multiple matches, but not per single full path.
+                if (count($matches) > 1) {
+                    throw new Exception('Encountered more than one match for ' . $localizedFileOrFolderPath . ' through ' . $rule->asRegExp());
+                }
+                if ($rule->targetsOnlyDirectories() && !$info['data']['is_directory']) {
+                    var_dump('-- SKIP - NOT DIR ------ ' . $rule->asRegExp() . ' => ' . $localizedFileOrFolderPath);
                     continue;
                 }
-                self::processRule($directory, $rule);
+                if ($rule->targetsMatching())
+                // TODO: This is working, but it is not localizing the expressions onto the local directory yet.
+                $info['data']['included'] = false;
+                //var_dump('-- MATCH --------------- ' . $rule->asRegExp() . ' => ' . $localizedFileOrFolderPath);
             }
         }
-        foreach ($directory as $fileOrFolderName => $info) {
-            // The search depth will make some directories not fetch their contents.
-            if ($info['is_directory'] && isset($info['contents'])) {
-                // Do not replace $directory[$fileOrFolderName] with $info, we're trying to create a reference here.
-                self::processGitAttributesFiles($directory[$fileOrFolderName]['contents']);
-            }
-        }
-    }
-
-    private static function processRule(array &$directory, PathMatcher $rule): void
-    {
-        var_dump($rule->asRegExp() . ' => ' . ($rule->targetsOnlyDirectories() ? 'only directories' : 'file/directory match'));
         return;
 
         // Rules can not look up, and they will always take the current directory of the .gitignore/.gitattributes file as their root.
@@ -138,19 +190,19 @@ final class PackageParser
             // The search depth will make some directories not fetch their contents.
             if (/** TODO: If rule influences more layers */ $info['is_directory'] && isset($info['contents'])) {
                 // Do not replace $directory[$fileOrFolderName] with $info, we're trying to create a reference here.
-                self::processRule($directory[$fileOrFolderName]['contents'], $rule);
+//                self::processRule($directory[$fileOrFolderName]['contents'], $rule);
             }
             //TODO: Add a rule localizer for rules that influence deeper paths specifically and not all paths.
         }
     }
 
-    private static function summarize(array $directory, int $currentDepth, int $maxDepth): array
+    private static function summarize(array $directory, int $maxDepth, int $currentDepth = 1): array
     {
         $contents = [];
         foreach ($directory as $fileOrFolderName => $info) {
             // The search depth will make some directories not fetch their contents.
-            if ($info['is_directory'] && isset($info['contents'])) {
-                $contents[$fileOrFolderName] = self::summarize($info['contents'], $currentDepth + 1, $maxDepth);
+            if ($info['is_directory'] && isset($info['contents']) && $currentDepth < $maxDepth) {
+                $contents[$fileOrFolderName] = self::summarize($info['contents'], $maxDepth, $currentDepth + 1);
             } else {
                 $contents[] = $fileOrFolderName;
             }
--- tests/PackageParserTest.php
+++ tests/PackageParserTest.php
@@ -12,13 +12,29 @@ final class PackageParserTest extends TestCase
     /** @test */
     public static function run_will_parse_the_working_directory_if_no_project_root_is_provided(): void
     {
-        // Update the README when updating this.
         self::assertSame(
             $result = [
                 'LICENSE',
                 'README.md',
                 'composer.json',
-                'src'
+                'src' => [
+                    'PackageParser.php',
+                    'PathMatcher.php',
+                    'PathMatcherComponent' => [
+                        'AnyCharacterExceptDirectoryIndicator.php',
+                        'AnyCharactersExceptDirectoryIndicator.php',
+                        'Character.php',
+                        'CharacterFromList.php',
+                        'CharacterListComponent' => [
+                            'CharacterListComponentInterface.php',
+                            'CharacterRange.php',
+                        ],
+                        'CurrentDirectoryAndAnyLevelSubDirectory.php',
+                        'DirectorySeparator.php',
+                        'PathMatcherComponentInterface.php',
+                    ],
+                    'RuleParser.php',
+                ],
             ],
             PackageParser::run(__DIR__ . DIRECTORY_SEPARATOR . '..', 5, 5)
         );
@@ -33,7 +49,12 @@ final class PackageParserTest extends TestCase
                 'LICENSE',
                 'README.md',
                 'composer.json',
-                'src'
+                'src' => [
+                    'PackageParser.php',
+                    'PathMatcher.php',
+                    'PathMatcherComponent',
+                    'RuleParser.php',
+                ],
             ],
             PackageParser::run(searchDepth: 2, resultDepth: 5)
         );
@@ -56,9 +77,23 @@ final class PackageParserTest extends TestCase
                 'LICENSE',
                 'README.md',
                 'composer.json',
-                'src'
+                'src' => [
+                    'PackageParser.php',
+                    'PathMatcher.php',
+                    'PathMatcherComponent' => [
+                        'AnyCharacterExceptDirectoryIndicator.php',
+                        'AnyCharactersExceptDirectoryIndicator.php',
+                        'Character.php',
+                        'CharacterFromList.php',
+                        'CharacterListComponent',
+                        'CurrentDirectoryAndAnyLevelSubDirectory.php',
+                        'DirectorySeparator.php',
+                        'PathMatcherComponentInterface.php',
+                    ],
+                    'RuleParser.php',
+                ],
             ],
-            PackageParser::run(searchDepth: 5, resultDepth: 2)
+            PackageParser::run(searchDepth: 6, resultDepth: 3)
         );
         self::assertSame(
             [
@@ -67,8 +102,21 @@ final class PackageParserTest extends TestCase
                 'composer.json',
                 'src'
             ],
-            PackageParser::run(searchDepth: 5)
+            PackageParser::run(searchDepth: 6)
         );
     }
 
+    /** @test */
+    public static function run_will_reduce_the_result_depth_to_the_minimum(): void
+    {
+        self::assertSame(
+            [
+                'LICENSE',
+                'README.md',
+                'composer.json',
+                'src'
+            ],
+            PackageParser::run()
+        );
+    }
 }
